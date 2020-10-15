2dbafbfb1ffa64672074a2255b4ad323e0688fa6
user54 <Rawan>
2020-10-15 13:22:42 +0300

    R plutus (colors) ^_^ 4

---
+++ app/src/main/java/com/falconssoft/plutusapp/WhereIGo.java
@@ -0,0 +1,15 @@
+package com.falconssoft.plutusapp;
+
+import android.os.Bundle;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WhereIGo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.where_i_go_activity);
+    }
+}
